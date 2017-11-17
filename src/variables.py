2ac38c33ebf4741b117a7097d6159a4ab40265ba
Prakhar Mishra <pmprakhargenius@gmail.com>
2017-11-18 00:27:42 +0530

    Update variables.py

--- src/variables.py
+++ src/variables.py
@@ -6,7 +6,7 @@ swiggy_url = "https://www.swiggy.com"
 swiggy_url_orders = "https://www.swiggy.com/my-account"
 
 # credentials
-phone = "8296603662"
-password = "mishra2121"
+phone = "<your registered number>"
+password = "<<your password>"
 
 
