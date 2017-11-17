73c443df207c0f7924edc4ba77c7f49463ae1c7d
prakhar <pmprakhargenius@gmail.com>
2017-11-18 00:26:29 +0530

    Adding templates

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo pip install -r requirements.txt
+sudo apt-get install python-bs4
+
---
+++ src/variables.py
@@ -0,0 +1,12 @@
+#!/bin/env python
+
+
+# request URL
+swiggy_url = "https://www.swiggy.com"
+swiggy_url_orders = "https://www.swiggy.com/my-account"
+
+# credentials
+phone = "8296603662"
+password = "mishra2121"
+
+
